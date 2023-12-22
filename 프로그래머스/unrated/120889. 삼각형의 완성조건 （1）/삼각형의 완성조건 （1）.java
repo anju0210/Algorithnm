8af2e612d110166c4ab866f794436d486c94722d
Hyoju An <129041719+anju0210@users.noreply.github.com>
2023-12-22 14:07:30 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.03 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int[] sides) {
         int answer = 1;
-        if(sides[0]>=sides[1]+sides[2] || sides[1]>=sides[0]+sides[2] || sides[2]>=sides[0]+sides[1]) answer = 2;
+        int puls[] = {sides[1]+sides[2],sides[0]+sides[2],sides[0]+sides[1]};
+        if(sides[0]>=puls[0] || sides[1]>=puls[1] || sides[2]>=puls[2]) answer = 2;
         return answer;
     }
 }
