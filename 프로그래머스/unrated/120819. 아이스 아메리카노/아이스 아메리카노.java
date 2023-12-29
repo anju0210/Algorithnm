6f567b3ae6b15c5fa761cbecaf9dfa5e5b27cb07
Hyoju An <129041719+anju0210@users.noreply.github.com>
2023-12-29 21:20:27 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,12 +1,6 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = {0,0};
-        int i = 1;
-        while(money>=5500){
-            money -= 5500;
-            answer[0]++;
-        }
-        answer[1] = money;
+        int[] answer = {money/5500,money%5500};
         return answer;
     }
 }
