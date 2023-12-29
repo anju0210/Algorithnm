0272de7b8c98cdbdd6b966d8aea49a46f774ef48
Hyoju An <129041719+anju0210@users.noreply.github.com>
2023-12-29 20:58:54 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -1,12 +1,8 @@
 class Solution {
     public int solution(int n, int k) {
-        int answer = 0;
-        int a = 0;
-        if(n>=10) a = n/10;
-        n *= 12000;
         k *= 2000;
-        k -= 2000*a;
-        answer = n+k;
-        return answer;
+        k -= 2000*(n/10);
+        n *= 12000;
+        return n+k;
     }
 }
