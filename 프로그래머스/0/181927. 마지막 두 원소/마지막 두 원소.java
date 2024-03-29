d4ba3caab3d08dd1f44f665f4fda4ad8cb4a41c0
Hyoju An <129041719+anju0210@users.noreply.github.com>
2024-03-29 23:01:46 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,10 +1,7 @@
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length+1];
-        if(num_list[num_list.length-1]>num_list[num_list.length-2])
-            answer[answer.length-1] = num_list[num_list.length-1]-num_list[num_list.length-2];
-        else
-            answer[answer.length-1] = num_list[num_list.length-1]*2;
+        answer[answer.length-1] = num_list[num_list.length-1]>num_list[num_list.length-2]? num_list[num_list.length-1]-num_list[num_list.length-2] : num_list[num_list.length-1]*2;
         for(int i=0; i<answer.length-1; i++){
             answer[i] = num_list[i];
         }
