7270659a87af2b6671c2e147715081181950aba4
Hyoju An <129041719+anju0210@users.noreply.github.com>
2024-07-04 14:04:09 +0900

    [level 0] Title: 수 조작하기 2, Time: 18.79 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,12 +1,6 @@
 def solution(numLog):
     answer = ''
+    d = {1:'w', -1:'s', 10:'d', -10:'a'}
     for i in range(1, len(numLog)):
-        if numLog[i]-numLog[i-1]==1:
-            answer += 'w'
-        elif numLog[i]-numLog[i-1]==-1:
-            answer += 's'
-        elif numLog[i]-numLog[i-1]==10:
-            answer += 'd'
-        elif numLog[i]-numLog[i-1]==-10:
-            answer += 'a'
+        answer += d[numLog[i]-numLog[i-1]]
     return answer
