9b7cda093839b3f1bf4bd08785cde25009c5a525
Hyoju An <129041719+anju0210@users.noreply.github.com>
2024-07-05 09:58:37 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.58 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -5,5 +5,5 @@ def solution(arr):
         if not stk or stk[len(stk)-1]<arr[i]:
             stk.append(arr[i])
             i += 1
-        else: stk.pop(len(stk)-1)
+        else: stk.pop()
     return stk
