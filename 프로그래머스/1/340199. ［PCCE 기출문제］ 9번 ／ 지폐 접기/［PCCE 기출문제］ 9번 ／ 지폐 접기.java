584737a1a5a2090967e1e6291da3d65c83fff1ef
Hyoju An <129041719+anju0210@users.noreply.github.com>
2024-09-21 09:48:32 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
-        int walMin = wallet[0]>wallet[1]? wallet[1] : wallet[0];
-        int walMax = wallet[0]>wallet[1]? wallet[0] : wallet[1];
-        int bilMin = bill[0]>bill[1]? bill[1] : bill[0];
-        int bilMax = bill[0]>bill[1]? bill[0] : bill[1];
         
-        while(bilMin>walMin || bilMax>walMax){
-            if(bill[0]>bill[1]) bill[0] /= 2;
-            else bill[1] /= 2;
-            bilMin = bill[0]>bill[1]? bill[1] : bill[0];
-            bilMax = bill[0]>bill[1]? bill[0] : bill[1];
-            
+        while(max(bill)>max(wallet) || min(bill)>min(wallet)){
+            bill[bill[0]>bill[1]? 0 : 1] /= 2;
             answer++;
         }
         return answer;
+    }
+    
+    private int max(int arr[]){
+        return arr[0]>arr[1]? arr[0] : arr[1];
+    }
+    private int min(int arr[]){
+        return arr[0]>arr[1]? arr[1] : arr[0];
     }
 }
