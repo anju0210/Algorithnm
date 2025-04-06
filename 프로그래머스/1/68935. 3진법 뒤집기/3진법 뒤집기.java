96f7be6fda3fb4b20a67a0aa1cfc23981a0763a0
Hyoju An <129041719+anju0210@users.noreply.github.com>
2025-04-06 22:47:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 14.99 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,29 +1,14 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        int len = 0;
-        int d=1;
-        while(d<=n){
-            d*=3;
-            len++;
+        String str = "";
+        while(n>0){
+            int r = n%3;
+            n /= 3;
+            str += r;
         }
         
-        int arr[] = new int[len];
-        for(int i=0; i<arr.length; i++){
-            int cnt = 0;
-            d/=3;
-            for(int j=1; j<=2; j++){
-                if(n-d<0) break;
-                n -= d;
-                cnt++;
-            }
-            arr[i] = cnt;
-        }
-        
-        int answer = arr[0]*1;
-        for(int i=1; i<arr.length; i++){
-            answer += arr[i]*Math.pow(3, i);
-        }
+        int answer = Integer.parseInt(str, 3);
         
         return answer;
     }
