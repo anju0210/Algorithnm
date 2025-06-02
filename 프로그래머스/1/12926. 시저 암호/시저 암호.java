ab8277fcf4a2b428aca2e76da88ae5fc28859585
Hyoju An <129041719+anju0210@users.noreply.github.com>
2025-06-02 14:57:28 +0900

    [level 1] Title: 시저 암호, Time: 127.12 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,18 +3,13 @@ class Solution {
         String answer = "";
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-            if(ch==' '){
-                answer += Character.toString(' ');
+            if(ch>='a'){
+                ch = (char)((ch-'a'+n)%26+'a');
             }
-            else if(ch<91&&ch+n>90){
-                answer += Character.toString('A'-1+n-(90-ch));
+            else if(ch>='A'){
+                ch = (char)((ch-'A'+n)%26+'A');
             }
-            else if(ch<123&&ch+n>122){
-                answer += Character.toString('a'-1+n-(122-ch));
-            }
-            else{
-                answer += Character.toString(ch+n);
-            }
+            answer += Character.toString(ch);
         }
         return answer;
     }
