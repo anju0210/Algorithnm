3910ff021a9ac76f8ef5bd33e292dbff66be673e
Hyoju An <129041719+anju0210@users.noreply.github.com>
2025-06-08 14:06:54 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,71 +1,35 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int person[] = {0,0,0};
-        int oneA = 0, twoA = 0, threeA = 3;
+        int oneA[] = {1, 2, 3, 4, 5};
+        int twoA[] = {2, 1, 2, 3, 2, 4, 2, 5};
+        int threeA[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int score[] = {0, 0, 0};
         
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == ++oneA){
-                person[0]++;
+            if(answers[i] == oneA[i%oneA.length]){
+                score[0]++;
             }
-            if(oneA==5){
-                oneA = 0;
+            if(answers[i] == twoA[i%twoA.length]){
+                score[1]++;
             }
-            
-            if(i%2==0){
-                if(answers[i]==2){
-                    person[1]++;
-                }
-            }
-            else{
-                if(answers[i]==++twoA){
-                    person[1]++;
-                }
-            }
-            if(twoA==1){
-                twoA++;
-            }
-            else if(twoA==5){
-                twoA = 0;
-            }
-            
-            if(answers[i]==threeA){
-                person[2]++;
-            }
-            if(i%2==1){
-                if(threeA==3){
-                    threeA = 1;
-                }
-                else if(threeA==2){
-                    threeA = 4;
-                }
-                else if(threeA==5){
-                    threeA = 3;
-                }
-                else{
-                    threeA++;
-                }
+            if(answers[i] == threeA[i%threeA.length]){
+                score[2]++;
             }
         }
         
-        int max = 0;
-        
-        for(int p : person){
-            max = max>p? max : p;
-        }
-        
-        int size=0;
-        for(int p : person){
-            if(max==p){
-                size++;
+        int max = Math.max(score[2], Math.max(score[1], score[0]));
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<score.length; i++){
+            if(max==score[i]){
+                list.add(i+1);
             }
         }
         
-        int answer[] = new int[size];
-        int idx = 0;
-        for(int i=0; i<person.length; i++){
-            if(person[i]==max){
-                answer[idx++] = i+1;
-            }
+        int answer[] = new int[list.size()];
+        
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
         }
         
         return answer;
